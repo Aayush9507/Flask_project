4a9dda5a3a1f5c330ab12ca14b00fb99b1827bee
aayush goyal <aayushgoyal9507@gmail.com>
2019-09-11 20:23:46 -0600

    Adding SQLite database and some end points for persistence

--- code/app.py
+++ code/app.py
@@ -1,28 +1,56 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 api = Api(app)
 app.secret_key = 'aayush'
 
+jwt = JWT(app, authenticate, identity)  # Auth
 
 items = []
 
 
-class Student(Resource):
+class Item(Resource):
 
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank !")
+
+    @jwt_required()
     def get(self, name):
-
         item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': item}, 202 if item else 404
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
+
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists. ".format(name)}, 400
-        data = request.get_json()
+
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+
+        return item
 
 
 class ItemList(Resource):
@@ -31,6 +59,7 @@ class ItemList(Resource):
         return {'items': items}
 
 
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 app.run(port=5000, debug=True)
--- code/security.py
+++ code/security.py
@@ -1,38 +1,14 @@
-users = [
-
-    {
-        'id': 1,
-        'username': 'aayush',
-        'password': 'asdf'
-    }
-]
-
-username_mapping = {'aayush': {
-
-    'id': 1,
-    'username': 'aayush',
-    'password': 'asdf'
-    }
-
-}
-
-userid_mapping = {1: {
-
-    'id': 1,
-    'username': 'aayush',
-    'password': 'asdf'
-    }
-
-}
+from user import User
+from werkzeug.security import safe_str_cmp
 
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
-    if user is not None and user.password == password:
+    user = User.find_by_username(username)
+    if user is not None and safe_str_cmp(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
 
